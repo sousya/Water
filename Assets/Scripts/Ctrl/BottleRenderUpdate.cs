cd25e41d1ff7635aec1cddd8e929ee8f585eb64a
sunhanpt <763737898@qq.com>
2025-03-13 00:52:33 +0800

    D: Stencil 效果调对了，还有几个细节要调。

--- Assets/Scripts/Ctrl/BottleRenderUpdate.cs
+++ Assets/Scripts/Ctrl/BottleRenderUpdate.cs
@@ -8,10 +8,12 @@ public class BottleRenderUpdate : MonoBehaviour
 {
     public WaterRenderUpdater[] waterRenders;
     public SkeletonGraphic[] Brooms;
+    public Transform WaterSpine;
 
     public int bottleIndex = 1;
 
     private Material _material;
+    private Vector3 _waterScale;
     public void Start()
     {
         _material = new Material(GetComponent<Image>().material);
@@ -29,7 +31,7 @@ public class BottleRenderUpdate : MonoBehaviour
             material.SetFloat("_Stencil", bottleIndex);
             material.SetFloat("_StencilOp", ((int)UnityEngine.Rendering.StencilOp.Keep) * 1.0f);
             material.SetFloat("_StencilComp", ((int)UnityEngine.Rendering.CompareFunction.Always) * 1.0f);
-            material.renderQueue = 3000 - 1;
+            material.renderQueue = 3000 + 1;
             skeletonGraphic.material = material;
             //skeletonGraphic.TrimRenderers();
             var skeletonSubmeshGraphics = skeletonGraphic.gameObject.GetComponentsInChildren<SkeletonSubmeshGraphic>();
@@ -38,13 +40,23 @@ public class BottleRenderUpdate : MonoBehaviour
                 skeletonSubmeshGraphic.material = material;
             }
         }
+        
+        _waterScale = WaterSpine.localScale;
+        var waterSpineMaterial = new Material(WaterSpine.GetComponent<SkeletonGraphic>().material);
+        waterSpineMaterial.SetFloat("_StencilWater", bottleIndex);
+        waterSpineMaterial.SetFloat("_StencilCompWater", (int)UnityEngine.Rendering.CompareFunction.Equal);
+        WaterSpine.GetComponent<SkeletonGraphic>().material = waterSpineMaterial;
     }
 
-    // public void Update()
-    // {
-    //     foreach (var waterSurface in waterSurfaces)
-    //     {
-    //         waterSurface.localRotation = Quaternion.Inverse(_bottleTransform.rotation);
-    //     }
-    // }
+    public void Update()
+    {
+        // foreach (var waterSurface in waterSurfaces)
+        // {
+        //     waterSurface.localRotation = Quaternion.Inverse(_bottleTransform.rotation);
+        // }
+        WaterSpine.localRotation = Quaternion.Inverse(transform.rotation);
+        transform.rotation.ToAngleAxis(out float angle, out _);
+        var oneDivCos = 1.0f / Mathf.Max(Mathf.Cos(angle * Mathf.Deg2Rad), 0.001f);
+        WaterSpine.localScale = new Vector3(oneDivCos *_waterScale.x, _waterScale.y, _waterScale.z);
+    }
 }

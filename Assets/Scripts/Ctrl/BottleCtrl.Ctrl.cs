e459b4ad8d4e8cca946b2ddf829a361e5522cac9
pengtong <pengtong@elex-tech.com>
2025-03-21 21:43:15 +0800

    dd

--- Assets/Scripts/Ctrl/BottleCtrl.Ctrl.cs
+++ Assets/Scripts/Ctrl/BottleCtrl.Ctrl.cs
@@ -9,8 +9,78 @@ using UnityEngine;
 using UnityEngine.UI;
 using static LevelCreateCtrl;
 
-public partial class BottleCtrl : IController, ICanSendEvent
+public class BottleCtrl : MonoBehaviour, IController, ICanSendEvent
 {
+    public bool isFinish, isFreeze, isClearHide, isNearHide, isPlayAnim, isSelect, isClearHideAnim;
+    public List<int> waters = new List<int>();
+    public List<bool> hideWaters = new List<bool>();
+    public List<WaterItem> waterItems = new List<WaterItem>();
+    public List<BottleWaterCtrl> waterImg = new List<BottleWaterCtrl>();
+    public List<Transform> spineNode = new List<Transform>();
+    public List<Transform> waterNode = new List<Transform>();
+    public Transform spineGo, modelGo, leftMovePlace, freezeGo;
+    public Animator bottleAnim, fillWaterGoAnim;
+    public SkeletonGraphic spine, finishSpine, freezeSpine;
+    public int maxNum = 4, limitColor = 0;
+    public Image ImgWaterTop, ImgWaterDown, ImgLimit;
+    public SkeletonGraphic nearHide, clearHide, thunder;
+    public bool isUp;
+    public GameObject finishGo;
+    public GameObject waterTopSurface;// 倒水的过程中，水面的最高高度不会超过这个线。
+    
+    public List<BottleRecord> moveRecords = new List<BottleRecord>();
+    
+    public int bottleIdx;
+    public int unlockClear = 0;
+    public Button bottle;
+    
+    // 存储每种情况的水的旋转角度。index表示瓶子剩余水的个数。
+    private Vector3[] _waterRotations = new Vector3[4];
+    private BottleRenderUpdate _bottleRenderUpdate;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        bottle.onClick.AddListener(OnSelected);
+        
+        // 计算瓶子的旋转角度(根据三角形公式推导)
+        var bottleCenterPos = this.gameObject.transform.position;
+        var sinEdge = Mathf.Abs(waterTopSurface.transform.position.x - bottleCenterPos.x);
+        _bottleRenderUpdate = bottleAnim.GetComponent<BottleRenderUpdate>();
+        
+        var waterRenderUpdaters = _bottleRenderUpdate.GetComponentsInChildren<WaterRenderUpdater>();
+        for (int i = waterRenderUpdaters.Length - 1; i >= 1; i--)
+        {
+            var position = waterRenderUpdaters[i].waterSurface[0].position;
+            
+            var cosEdge = Mathf.Abs(waterTopSurface.transform.position.y - position.y);
+            _waterRotations[i] = GetBottleRotation(sinEdge, cosEdge);
+        }
+        // 倒完水使用120度写死角度。
+        //_waterRotations[0] = Quaternion.Euler(0, 0, -120); 
+        _waterRotations[0] = new Vector3(0, 0, -120);
+    }
+    
+    private void LateUpdate()
+    {
+        fillWaterGoAnim.transform.localRotation = Quaternion.Inverse(fillWaterGoAnim.transform.parent.rotation);
+    }
+    
+    private void OnSelected()
+    {
+        if(!LevelManager.Instance.isPlayAnim && !LevelManager.Instance.isPlayFxAnim)
+        {
+            GameCtrl.Instance.OnSelect(this);
+        }
+    }
+    
+    private Vector3 GetBottleRotation(float sinEdge, float cosEdge)
+    {
+        float angle = Mathf.Atan(sinEdge / cosEdge);
+        angle = Mathf.PI / 2 - angle;
+        return new Vector3(0, 0, -angle * Mathf.Rad2Deg);
+    }
+
     public IArchitecture GetArchitecture()
     {
         return GameMainArc.Interface;
@@ -924,10 +994,7 @@ public partial class BottleCtrl : IController, ICanSendEvent
         var bottleRenderUpdate = bottleAnim.GetComponent<BottleRenderUpdate>();
         bottleRenderUpdate.SetMoveBottleRenderState(true);
         
-        if(useColor >= 1000)
-        {
-            bottleAnim.Play("BottleItemOut");
-        }
+        
         modelGo.transform.DOMove(other.leftMovePlace.position, 0.46f).SetEase(Ease.Linear).OnComplete(() =>
         {
             SetDownWaterSp(useColor);
@@ -935,26 +1002,30 @@ public partial class BottleCtrl : IController, ICanSendEvent
             {
                 PlayWaterDown();
 
-                modelGo.transform.DORotateQuaternion(_waterRotations[leftWater], 0.62f).SetEase(Ease.Linear).OnComplete(() =>
+                //modelGo.transform.DORotate(_waterRotations[leftWater], 0.62f).SetEase(Ease.Linear).OnComplete(() =>
+                modelGo.transform.DORotate(_waterRotations[leftWater], 10.0f).SetEase(Ease.Linear).OnComplete(() =>
                 {
                     modelGo.transform.DOLocalMove(Vector3.zero, 0.46f).SetEase(Ease.Linear).OnComplete(() =>
                     {
                         LevelManager.Instance.isPlayAnim = false;
                         bottleRenderUpdate.SetMoveBottleRenderState(false);
                     });
+                    modelGo.transform.DORotate(Vector3.zero, 0.14f).SetEase(Ease.Linear).OnComplete(null);
                 });
             }
             else
             {
-                modelGo.transform.DOMove(other.leftMovePlace.position, 0.4f).SetEase(Ease.Linear).OnComplete(() =>
+                modelGo.transform.DORotate(new Vector3(0, 0, -45), 0.4f).SetEase(Ease.Linear).OnComplete(() =>
                 {
                     modelGo.transform.DOLocalMove(Vector3.zero, 0.46f).SetEase(Ease.Linear).OnComplete(() =>
                     {
                         LevelManager.Instance.isPlayAnim = false;
                         bottleRenderUpdate.SetMoveBottleRenderState(false);
                     });
+                    modelGo.transform.DORotate(Vector3.zero, 0.14f).SetEase(Ease.Linear).OnComplete(null);
                 });
             }
+            
         });
     }
 
